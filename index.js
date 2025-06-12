9221c73228df16f0f97e86dfeb05c218d5ea427d
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-06-12 14:46:59 +0600

    B2B Server side initialized

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("server working");
+});
+
+app.listen(port, () => {
+  console.log("server running on ", port);
+});
